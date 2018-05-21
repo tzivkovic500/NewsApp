cd58164062ad9a3fd6517b9cbe62c25bfb7c6e4f
tea <tzivkovic500@gmail.com>
2018-05-21 17:27:13 +0200

    QueryUtils2

--- app/src/main/java/com/example/android/newsapp/QueryUtils.java
+++ app/src/main/java/com/example/android/newsapp/QueryUtils.java
@@ -189,7 +189,7 @@ public final class QueryUtils {
             // If an error is thrown when executing any of the above statements in the "try" block,
             // catch the exception here, so the app doesn't crash. Print a log message
             // with the message from the exception.
-            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+            Log.e("QueryUtils", "Problem parsing the news JSON results", e);
         }
         // Return the list of newsInfo
         return newsInfo;
